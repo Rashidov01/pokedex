046d7483f214599d94b781cc70e54fd5f3d41816
Abdulloh <72493727+Rashidov01@users.noreply.github.com>
2021-09-07 16:24:59 +0500

    finished

--- js/main.js
+++
@@ -1,9 +0,0 @@
-var elInput = document.querySelector(".js-input");
-var elOutput = document.querySelector(".js-output");
-var elSubmitBtn = document.querySelector(".js-submit");
-var elForm = document.querySelector(".js-form");
-
-elForm.addEventListener("submit", function (e) {
-  e.preventDefault();
-
-})
